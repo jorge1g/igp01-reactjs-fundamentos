8e1adb25af147cf9f0cd0e6456db0ab171f1094e
Jorge Gonzaga <jsgonzaga@uol.com.br>
2022-09-18 14:30:52 -0300

    atualizado propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { Post } from './Post';
+import { Post } from "./Post";
 
 export function App() {
   return (
     <>
-      <Post />
+      <Post
+        author="Jorge Gonzaga"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum quos, veritatis incidunt atque tempore ex odio minima, pariatur minus consequatur sequi temporibus necessitatibus quisquam numquam adipisci repellendus velit natus enim."
+      />
+      <Post 
+      author="Diego Gonzaga"
+      content="Jogar é muito bom"
+      />
     </>
-  )
+  );
 }
-
