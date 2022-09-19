cbe914d3f6552fed10934c2be3fabfda2865ca4b
Jorge Gonzaga <jsgonzaga@uol.com.br>
2022-09-19 17:30:54 -0300

    estilizações do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,10 +2,10 @@ import stylesPost from './Post.module.css';
 
 export function Post() {
     return (
-        <article classname={stylesPost.Post}>
+        <article className={stylesPost.post}>
             <header>
                 <div className={stylesPost.author}>
-                    <img classname={stylesPost.avatar} src="https://avatars.githubusercontent.com/u/54822837?v=4" />
+                    <img className={stylesPost.avatar} src="https://avatars.githubusercontent.com/u/54822837?v=4" />
                     <div className={stylesPost.authorInfo}>
                         <strong>Jorge Luis</strong>
                         <span>Developer</span>
@@ -16,12 +16,16 @@ export function Post() {
                 <time title="11 de maio às 08:15h" dateTime="2002-05-11 08:13:35">Publicado há 1h</time>
             </header>
 
-            <div classname={stylesPost.content}>
+            <div className={stylesPost.content}>
                 
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 <a href="">jane.design/doctorcare</a></p>
-                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                <p><a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto </a>{' '}
+                    <a>#nlw </a> {' '}
+                    <a>#rocketseat</a> {' '}
+                </p>
                 
             </div>
         
