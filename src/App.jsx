eb7d33965f6d82134d0758e342c9ee46372389d5
Jorge Gonzaga <jsgonzaga@uol.com.br>
2022-09-18 21:04:52 -0300

    css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import './styles.css';
 
 export function App() {
   return (
-    <>
+    <div>
+      <Header />
+      
       <Post
         author="Jorge Gonzaga"
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum quos, veritatis incidunt atque tempore ex odio minima, pariatur minus consequatur sequi temporibus necessitatibus quisquam numquam adipisci repellendus velit natus enim."
@@ -11,6 +16,6 @@ export function App() {
       author="Diego Gonzaga"
       content="Jogar é muito bom"
       />
-    </>
+    </div>
   );
 }
